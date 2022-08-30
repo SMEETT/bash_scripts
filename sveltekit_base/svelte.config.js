b57021c17d181c6443f11c6c5586120f448b9acc
Boris Fries <tbr@tutamail.com>
2022-08-30 18:38:17 +0200

    added sveltekit_base to repo

---
+++ sveltekit_base/svelte.config.js
@@ -0,0 +1,19 @@
+import preprocess from 'svelte-preprocess';
+import adapter from '@sveltejs/adapter-node';
+
+/** @type {import('@sveltejs/kit').Config} */
+const config = {
+	// Consult https://github.com/sveltejs/svelte-preprocess
+	// for more information about preprocessors
+	preprocess: [
+		preprocess({
+			postcss: true
+		})
+	],
+
+	kit: {
+		adapter: adapter({ out: 'build' })
+	}
+};
+
+export default config;
