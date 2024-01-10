49d4960cc0194f08980c18a47c8cb4ccca25dddb
Ben <tbr@tutamail.com>
2024-01-10 14:41:23 +0100

    removed wrong folder from repo

--- sveltekit_base/src/routes/+page.svelte
+++
@@ -1,9 +0,0 @@
-<script>
-	import { PUBLIC_strapiURL } from "$env/static/public";
-	// data object that is going to be populated by our corresponding +page.server.ts
-	// export let data;
-	// console.log(data);
-</script>
-
-<a href="/about">About</a>
-{PUBLIC_strapiURL}
--- sveltekit_base/src/routes/about/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script>
-	// export let data;
-	// console.log(data.data.id);
-	// let recData = data.data.attributes;
-</script>
-
-<h1>ABOUT</h1>
-<a href="/">Home</a>
